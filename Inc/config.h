eb6f3330d210f7b1b07c84ca5c37f7e3d30d0157
Cranberry Jus <83450678+Buster06@users.noreply.github.com>
2026-03-06 15:21:59 +0100

    gametrak + nunchuk

--- Inc/config.h
+++ Inc/config.h
@@ -587,7 +587,7 @@
 //TODO ADD VALIDATION
 #ifdef VARIANT_TRANSPOTTER
   #define FLASH_WRITE_KEY     0x1009    // Flash memory writing key. Change this key to ignore the input calibrations from the flash memory and use the ones in config.h
-  // #define CONTROL_GAMETRAK
+  #define CONTROL_GAMETRAK
   // #define SUPPORT_LCD
   #define SUPPORT_NUNCHUK
   #define GAMETRAK_CONNECTION_NORMAL    // for normal wiring according to the wiki instructions
